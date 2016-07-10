d06ab9d1781072af537a625a00dc89b9589c0b91
CosmicDan <dconnolly.au@gmail.com>
2016-07-10 21:47:28 +1000

    Replace TAB chars with 4x space

--- src/com/cosmicdan/reignadditionals/asmhelper/ObfRemappingClassWriter.java
+++ src/com/cosmicdan/reignadditionals/asmhelper/ObfRemappingClassWriter.java
@@ -8,45 +8,45 @@ import org.objectweb.asm.ClassWriter;
  */
 public class ObfRemappingClassWriter extends ClassWriter
 {
-	public ObfRemappingClassWriter(int flags)
-	{
-		super(flags);
-	}
+    public ObfRemappingClassWriter(int flags)
+    {
+        super(flags);
+    }
 
-	@Override
-	protected String getCommonSuperClass(final String type1, final String type2)
-	{
-		Class<?> c, d;
-		ClassLoader classLoader = getClass().getClassLoader();
-		try
-		{
-			c = Class.forName(ObfHelper.toDeobfClassName(type1.replace('/', '.')), false, classLoader);
-			d = Class.forName(ObfHelper.toDeobfClassName(type2.replace('/', '.')), false, classLoader);
-		}
-		catch (Exception e)
-		{
-			throw new RuntimeException(e);
-		}
-		if (c.isAssignableFrom(d))
-		{
-			return type1;
-		}
-		if (d.isAssignableFrom(c))
-		{
-			return type2;
-		}
-		if (c.isInterface() || d.isInterface())
-		{
-			return "java/lang/Object";
-		}
-		else
-		{
-			do
-			{
-				c = c.getSuperclass();
-			}
-			while (!c.isAssignableFrom(d));
-			return ObfHelper.toObfClassName(c.getName()).replace('.', '/');
-		}
-	}
+    @Override
+    protected String getCommonSuperClass(final String type1, final String type2)
+    {
+        Class<?> c, d;
+        ClassLoader classLoader = getClass().getClassLoader();
+        try
+        {
+            c = Class.forName(ObfHelper.toDeobfClassName(type1.replace('/', '.')), false, classLoader);
+            d = Class.forName(ObfHelper.toDeobfClassName(type2.replace('/', '.')), false, classLoader);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+        if (c.isAssignableFrom(d))
+        {
+            return type1;
+        }
+        if (d.isAssignableFrom(c))
+        {
+            return type2;
+        }
+        if (c.isInterface() || d.isInterface())
+        {
+            return "java/lang/Object";
+        }
+        else
+        {
+            do
+            {
+                c = c.getSuperclass();
+            }
+            while (!c.isAssignableFrom(d));
+            return ObfHelper.toObfClassName(c.getName()).replace('.', '/');
+        }
+    }
 }
--- src/com/cosmicdan/reignadditionals/core/transformers/TransformBlockLiquid.java
+++ src/com/cosmicdan/reignadditionals/core/transformers/TransformBlockLiquid.java
@@ -7,44 +7,44 @@ import com.cosmicdan.reignadditionals.asmhelper.ASMHelper;
 
 public class TransformBlockLiquid extends AbstractInjectJumpTransformer {
 
-	public TransformBlockLiquid() {
-		// hook at that last "else if (l <= 4)" in BlockLiquid
-		super(Opcodes.IF_ICMPGT);
-	}
+    public TransformBlockLiquid() {
+        // hook at that last "else if (l <= 4)" in BlockLiquid
+        super(Opcodes.IF_ICMPGT);
+    }
 
-	@Override
-	public String getTargetClass() {
-		return "net.minecraft.block.BlockLiquid";
-	}
+    @Override
+    public String getTargetClass() {
+        return "net.minecraft.block.BlockLiquid";
+    }
 
-	@Override
-	public String getTargetMethod() {
-		return "func_149805_n";
-	}
+    @Override
+    public String getTargetMethod() {
+        return "func_149805_n";
+    }
 
-	@Override
-	public String getTargetDesc() {
-		return "(Lnet/minecraft/world/World;III)V";
-	}
+    @Override
+    public String getTargetDesc() {
+        return "(Lnet/minecraft/world/World;III)V";
+    }
 
-	@Override
-	public String getReason() {
-		return "Option to prevent cobble generating from lava + water mixing";
-	}
-	
-	@Override
-	public JumpInsnNode getTargetNode(MethodNode m) {
-		return (JumpInsnNode) ASMHelper.findLastInstructionWithOpcode(m, targetOp);
-	}
+    @Override
+    public String getReason() {
+        return "Option to prevent cobble generating from lava + water mixing";
+    }
+    
+    @Override
+    public JumpInsnNode getTargetNode(MethodNode m) {
+        return (JumpInsnNode) ASMHelper.findLastInstructionWithOpcode(m, targetOp);
+    }
 
-	@Override
-	public InsnList injectOps(InsnList toInject) {
-		// call CobbleMixEvent#cobbleMix, if it returns true then goto to label that the original jump requested
-		toInject = new InsnList();
+    @Override
+    public InsnList injectOps(InsnList toInject) {
+        // call CobbleMixEvent#cobbleMix, if it returns true then goto to label that the original jump requested
+        toInject = new InsnList();
         //toInject.add(new VarInsnNode(Opcodes.ALOAD, 1)); // push first parameter (World p_149805_1_) onto stack
         //toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "com/cosmicdan/reignadditionals/events/BlockLiquidEvent", "cobbleMix", "(Lnet/minecraft/world/World;)Z", false));
-		toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "com/cosmicdan/reignadditionals/events/BlockLiquidEvent", "cobbleMix", "()Z", false));
+        toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "com/cosmicdan/reignadditionals/events/LiquidMixingEvents", "cobbleMix", "()Z", false));
         toInject.add(new JumpInsnNode(Opcodes.IFEQ, targetNode.label));
         return toInject; 
-	}
+    }
 }
