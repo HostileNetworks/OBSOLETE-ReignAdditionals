e20ec563b9f7daf0a7de05833193d35a5e959811
CosmicDan <dconnolly.au@gmail.com>
2016-12-21 00:19:01 +1100

    Fix false login-gui text appearing (for fake or other players instead of current player)

--- src/com/cosmicdan/reignadditionals/events/EntityEvents.java
+++ src/com/cosmicdan/reignadditionals/events/EntityEvents.java
@@ -3,6 +3,7 @@ package com.cosmicdan.reignadditionals.events;
 import com.cosmicdan.reignadditionals.client.gui.GuiTextOverlay;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 
@@ -10,7 +11,8 @@ public class EntityEvents {
     @SubscribeEvent
     public void onPlayerLoggedIn(EntityJoinWorldEvent event) {
         if ((event.world.isRemote) && (event.entity instanceof EntityPlayer)) {
-            GuiTextOverlay.triggerPlayerJoin();
+            if (Minecraft.getMinecraft().thePlayer.getUniqueID().equals(((EntityPlayer)event.entity).getUniqueID()))
+                GuiTextOverlay.triggerPlayerJoin();
         }
     }
 }
