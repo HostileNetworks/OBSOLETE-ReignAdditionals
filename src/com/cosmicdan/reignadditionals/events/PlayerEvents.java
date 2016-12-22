d57338bc15d7a2308fb6c364b7d52b4457a76808
CosmicDan <dconnolly.au@gmail.com>
2016-12-22 22:25:18 +1100

    Revert "Compatibility fix for other mods that depend on hand being able to harvest wood (e.g. Storage Drawers)"
    This reverts commit a98bd9a43f1dedec848a252559a9c13492d53baf.

--- src/com/cosmicdan/reignadditionals/events/PlayerEvents.java
+++ src/com/cosmicdan/reignadditionals/events/PlayerEvents.java
@@ -14,14 +14,28 @@ import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 public class PlayerEvents {
+    
+    @SubscribeEvent
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (ModConfig.ALLOW_BREAKING_WITHOUT_TOOL) return;
+        if (event.entityPlayer.capabilities.isCreativeMode) return;
+        if (event.action == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK) {
+            Block tryingToHarvest = event.world.getBlock(event.x, event.y, event.z); 
+            int blockMeta = event.world.getBlockMetadata(event.x, event.y, event.z);
+            if (!tryHarvest(tryingToHarvest, blockMeta, event.entityPlayer, false)) {
+                event.setCanceled(true);
+            }
+        }
+    }
+
     @SubscribeEvent
     public void onBreakSpeed(BreakSpeed event) {
         if (ModConfig.ALLOW_BREAKING_WITHOUT_TOOL) return;
         if (event.entityPlayer.capabilities.isCreativeMode) return;
         Block tryingToHarvest = event.entityPlayer.worldObj.getBlock(event.x, event.y, event.z);
         int blockMeta = event.entityPlayer.worldObj.getBlockMetadata(event.x, event.y, event.z);
-        if (!tryHarvest(tryingToHarvest, blockMeta, event.entityPlayer)) {
-            event.newSpeed = 0.00001f;
+        if (!tryHarvest(tryingToHarvest, blockMeta, event.entityPlayer, true)) {
+            event.setCanceled(true);
         }
     }
     
@@ -31,19 +45,25 @@ public class PlayerEvents {
      * 
      */
 
-    private boolean tryHarvest(Block block, int blockMeta, EntityPlayer entityPlayer) {
+    private boolean tryHarvest(Block block, int blockMeta, EntityPlayer entityPlayer, boolean silent) {
         if (block.getMaterial() == Material.wood)
-            return letPlayerUseTool(block, blockMeta, entityPlayer, "axe");
+            return letPlayerUseTool(block, blockMeta, entityPlayer, "axe", silent);
         if (block.getMaterial() == Material.rock)
-            return letPlayerUseTool(block, blockMeta, entityPlayer, "pickaxe");
+            return letPlayerUseTool(block, blockMeta, entityPlayer, "pickaxe", silent);
         // allow the player to punch-break/harvest anything that isn't explicitly prevented
         return true;
     }
 
-    private boolean letPlayerUseTool(Block block, int blockMeta, EntityPlayer entityPlayer, String toolClass) {
-        if (entityPlayer.getHeldItem() != null)
+    private boolean letPlayerUseTool(Block block, int blockMeta, EntityPlayer entityPlayer, String toolClass, boolean silent) {
+        if (entityPlayer.getHeldItem() != null) {
+            silent = true;
             if ((block.canHarvestBlock(entityPlayer, blockMeta)) && (entityPlayer.getHeldItem().getItem().getHarvestLevel(entityPlayer.getHeldItem(), toolClass) != -1))
                     return true;
+        }
+        if (!silent) {
+            entityPlayer.attackEntityFrom(DamageSource.generic, 0.1f);
+            entityPlayer.addChatMessage(new ChatComponentText("Ouch! It seems I need the right tool for the job..."));
+        }
         return false;
     }
 }
