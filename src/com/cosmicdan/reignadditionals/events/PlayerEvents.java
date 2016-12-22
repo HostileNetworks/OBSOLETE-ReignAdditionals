fb422a6c604a509081f394be2f6b339f10bd2457
CosmicDan <dconnolly.au@gmail.com>
2016-12-23 00:14:38 +1100

    Add ore dictionary checks to harvest restrictions

--- src/com/cosmicdan/reignadditionals/events/PlayerEvents.java
+++ src/com/cosmicdan/reignadditionals/events/PlayerEvents.java
@@ -7,10 +7,12 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 public class PlayerEvents {
@@ -22,7 +24,7 @@ public class PlayerEvents {
         if (event.action == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK) {
             Block tryingToHarvest = event.world.getBlock(event.x, event.y, event.z); 
             int blockMeta = event.world.getBlockMetadata(event.x, event.y, event.z);
-            if (!tryHarvest(tryingToHarvest, blockMeta, event.entityPlayer, false)) {
+            if (!tryHarvest(tryingToHarvest, blockMeta, event.entityPlayer)) {
                 event.setCanceled(true);
             }
         }
@@ -34,7 +36,7 @@ public class PlayerEvents {
         if (event.entityPlayer.capabilities.isCreativeMode) return;
         Block tryingToHarvest = event.entityPlayer.worldObj.getBlock(event.x, event.y, event.z);
         int blockMeta = event.entityPlayer.worldObj.getBlockMetadata(event.x, event.y, event.z);
-        if (!tryHarvest(tryingToHarvest, blockMeta, event.entityPlayer, true)) {
+        if (!tryHarvest(tryingToHarvest, blockMeta, event.entityPlayer)) {
             event.setCanceled(true);
         }
     }
@@ -45,24 +47,27 @@ public class PlayerEvents {
      * 
      */
 
-    private boolean tryHarvest(Block block, int blockMeta, EntityPlayer entityPlayer, boolean silent) {
-        if (block.getMaterial() == Material.wood)
-            return letPlayerUseTool(block, blockMeta, entityPlayer, "axe", silent);
-        if (block.getMaterial() == Material.rock)
-            return letPlayerUseTool(block, blockMeta, entityPlayer, "pickaxe", silent);
+    private boolean tryHarvest(Block block, int blockMeta, EntityPlayer entityPlayer) {
+        if (block.getMaterial() == Material.wood) {
+            for (int oreId : OreDictionary.getOreIDs(new ItemStack(block))) {
+                if (OreDictionary.getOreName(oreId).equals("logWood"))
+                    return letPlayerUseTool(block, blockMeta, entityPlayer, "axe");
+            }
+        }
+        if (block.getMaterial() == Material.rock) {
+            for (int oreId : OreDictionary.getOreIDs(new ItemStack(block))) {
+                if (OreDictionary.getOreName(oreId).equals("stone"))
+                    return letPlayerUseTool(block, blockMeta, entityPlayer, "pickaxe");
+            }
+        }
         // allow the player to punch-break/harvest anything that isn't explicitly prevented
         return true;
     }
 
-    private boolean letPlayerUseTool(Block block, int blockMeta, EntityPlayer entityPlayer, String toolClass, boolean silent) {
+    private boolean letPlayerUseTool(Block block, int blockMeta, EntityPlayer entityPlayer, String toolClass) {
         if (entityPlayer.getHeldItem() != null) {
-            silent = true;
             if ((block.canHarvestBlock(entityPlayer, blockMeta)) && (entityPlayer.getHeldItem().getItem().getHarvestLevel(entityPlayer.getHeldItem(), toolClass) != -1))
                     return true;
-        }
-        if (!silent) {
-            entityPlayer.attackEntityFrom(DamageSource.generic, 0.1f);
-            entityPlayer.addChatMessage(new ChatComponentText("Ouch! It seems I need the right tool for the job..."));
         }
         return false;
     }
